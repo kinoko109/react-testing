65bbc4e526a84e2d16bcc10102b625b7c9d50634
松岡龍紀 <tatsuaki_matsuoka@bita.jp>
2020-11-15 14:41:09 +0900

    update: コメント修正

--- src/UseEffectRender/index.test.js
+++ src/UseEffectRender/index.test.js
@@ -5,8 +5,10 @@ import UseEffectRender from './index';
 describe('', () => {
   it('', async () => {
     render(<UseEffectRender />);
+    // I amが当てはまるテキストがあるかを探す
     expect(screen.queryByText(/I am/)).toBeNull();
     console.log(await screen.findByText(/I am/));
+    // findByTextは、レスポンスが返ってきてくれるまで待ってから処理してくれる（最大４秒？）
     expect(await screen.findByText(/I am/)).toBeInTheDocument();
   });
 });
